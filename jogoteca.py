a99a33dcffa34b96b5895663bb4ba0dcf9429a9d
davittiw <lucctyte@gmail.com>
2025-04-12 17:28:25 -0300

    start commmit

---
+++ jogoteca.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+app = Flask(__name__)
+@app.route('/')
+def ola():
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('Minecraft', 'Build', 'Xbox')
+    jogo3 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+app.run()
